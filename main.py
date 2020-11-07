8cf66d7483956fab6f2824a2734b138694772e6d
MichGur <michael@thegur.net>
2020-11-08 00:16:40 +0200

    added item posting

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import sqlite3
@@ -8,27 +10,45 @@ help_msg = 'אהלן, אני קניבוט.\n פקודות:\n"עזרה": לקבל
 send_cmd = 'שלח'
 write_cmd = 'תרשום'
 write_msg = '📝'
+writing = False
+finish_cmd = 'זהו'
+failed_msg = 'אופס חלה אצלי תקלה'
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
+    global writing
     incoming_msg = request.values.get('Body', '').lower()
     resp = MessagingResponse()
     msg = resp.message()
-    if help_cmd in incoming_msg: msg.body(help_msg)
-    elif send_cmd in incoming_msg:
-        conn = sqlite3.connect('./knibot.db')
-        c = conn.cursor()
-        c.execute('SELECT * FROM items')
-        items = c.fetchall()
-        msg.body('\n'.join(str(i[0]) + ': ' + i[2] + ' (' + i[1] + ')' for i in items))
-        conn.close()
-    else: msg.body(incoming_msg + ' you')
-    # return a quote
-    # r = requests.get('https://api.quotable.io/random')
-    # if r.status_code == 200:
-    # msg.body(quote)
-    # msg.media('https://cataas.com/cat')
+    try:
+        if help_cmd in incoming_msg:
+            msg.body(help_msg)
+        elif send_cmd in incoming_msg:
+            conn = sqlite3.connect('./knibot.db')
+            c = conn.cursor()
+            c.execute('SELECT * FROM items')
+            items = c.fetchall()
+            msg.body('\n'.join(str(i) + ': ' + r[0] + ' (' + str(r[1]) + ')' for i, r in enumerate(items)))
+            conn.close()
+        elif write_cmd in incoming_msg:
+            writing = True
+            msg.body(write_msg)
+        elif writing:
+            if finish_cmd in incoming_msg:
+                writing = False
+                msg.body('אוקי')
+            else:
+                request_by = int(re.search(r'\d+', request.values.get('From')).group())
+                conn = sqlite3.connect('./knibot.db')
+                c = conn.cursor()
+                c.execute('INSERT INTO items (name, request_by) VALUES (?, ?)',
+                          (incoming_msg, request_by))
+                conn.commit()
+        else:
+            msg.body(incoming_msg + ' you')
+    except Exception as e:
+        msg.body(failed_msg + ' ' + str(e))
     return str(resp)
 
 
@@ -37,7 +57,6 @@ if __name__ == '__main__':
     # c = conn.cursor()
     # c.execute('DROP TABLE IF EXISTS items')
     # c.execute('CREATE TABLE items ('
-    #           'id INTEGER PRIMARY KEY,'
     #           'name TEXT NOT NULL,'
     #           'request_by INTEGER)')
     # c.execute('SELECT * FROM items')
