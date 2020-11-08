6c031e1b79fda226a3dc28849c7f8dc57cd32ad6
MichGur <michael@thegur.net>
2020-11-08 17:41:58 +0200

    added re parsing for list creation

--- groups.py
+++ groups.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+import prompts
+
 
 class ClosingConnection:
     def __init__(self, path, commit):
@@ -27,67 +29,105 @@ class knibot_db:
         return ClosingConnection(cls.path, commit)
 
 
-def create_list(list_name, created_by=0):
+def create_list(user, list_name):
     with knibot_db.connect(commit=True) as conn:
         c = conn.cursor()
+        c.execute('SELECT id FROM lists WHERE id IN ('
+                  'SELECT list_id FROM listsForUsers WHERE user_id=?)'
+                  'AND name=?', (user, list_name))
+        if c.fetchone() is not None:
+            raise ValueError(prompts.already_exists_err % list_name)
         c.execute('INSERT INTO lists (name) VALUES ("%s")' % list_name)
-        if created_by != 0:
+        if user != 0:
             c.execute('INSERT INTO listsForUsers (list_id, user_id, admin) VALUES (?, ?, 1)',
-                      (c.lastrowid, created_by))
+                      (c.lastrowid, user))
         return c.lastrowid
 
 
-def add_user_to_list(user, list_name, admin=False):
+def add_users_to_list(list_name, users, admins=None):
+    if admins is None:
+        admins = []
     with knibot_db.connect(commit=True) as conn:
         c = conn.cursor()
         c.execute('SELECT id FROM lists WHERE name="%s"' % list_name)
-        c.execute('INSERT INTO listsForUsers (list_id, user_id, admin) VALUES ('
-                  '?, ?, ?)', (c.fetchall()[0][0], user, 1 if admin else 0))
+        working_list = c.fetchone()[0]
+        values = ', '.join("(%i, %i, %i)" % (working_list, u, 1 if u in admins else 0) for u in users)
+        c.execute('INSERT INTO listsForUsers (list_id, user_id, admin) VALUES %s' % values)
 
 
 def set_working_list(user, list_name):
     with knibot_db.connect(commit=True) as conn:
         c = conn.cursor()
-        c.execute('SELECT id FROM lists WHERE name="%s"' % list_name)
+        c.execute('SELECT id FROM lists WHERE id IN ('
+                  'SELECT list_id FROM listsForUsers WHERE user_id=?)'
+                  'AND name=?', (user, list_name))
+        working_list = c.fetchone()
+        if working_list is None:
+            raise ValueError(prompts.not_exists_err % list_name)
         c.execute('REPLACE INTO workingLists (user_id, list_id) VALUES (?, ?)',
-                  (user, c.fetchone()[0]))
+                  (user, working_list[0]))
 
 
-if __name__ == '__main__':
-    # create_list('assholes')
-    # add_user_to_list(1411, 'assholes', True)
-    # add_user_to_list(487834, 'assholes', False)
-    set_working_list(1411, 'friends')
-    set_working_list(1411, 'assholes')
-    set_working_list(487834, 'friends')
+def get_working_list(user, c):
+    c.execute('SELECT list_id FROM workingLists WHERE user_id=%i' % user)
+    working_list = c.fetchone()
+    if working_list is None:
+        raise LookupError('no working list for user %i' % user)
+    return working_list[0]
+
+
+def add_items(user, *items):
     with knibot_db.connect(commit=True) as conn:
         c = conn.cursor()
-        # c.execute('DROP TABLE IF EXISTS lists')
-        # c.execute('DROP TABLE IF EXISTS listsForUsers')
-        # c.execute('DROP TABLE IF EXISTS workingLists')
-        # c.execute('CREATE TABLE lists ('
-        #           'id INTEGER PRIMARY KEY,'
-        #           'name TEXT NOT NULL)')
-        # c.execute('CREATE TABLE listsForUsers ('
-        #           'list_id INTEGER,'
-        #           'user_id INTEGER,'
-        #           'admin INTEGER,'
-        #           'FOREIGN KEY(list_id) REFERENCES lists(id))')
-        # c.execute('INSERT INTO lists (name) VALUES ("friends")')
-        # c.execute('INSERT INTO listsForUsers (list_id, user_id) VALUES (0, 972502057283)')
-        # # select all available lists for a user
-        # c.execute('SELECT name FROM lists WHERE id IN ('
-        #           'SELECT list_id FROM listsForUsers WHERE user_id=972502057283)')
-        # # select all users in a list
-        # c.execute('SELECT user_id FROM listsForUsers WHERE list_id=0')
-        # c.execute('CREATE TABLE items ('
-        #           'name TEXT NOT NULL,'
-        #           'list_id INTEGER,'
-        #           'request_by INTEGER,'
-        #           'FOREIGN KEY(list_id) REFERENCES lists(id))')
-        # c.execute('CREATE TABLE workingLists ('
-        #           'user_id INTEGER UNIQUE,'
-        #           'list_id INTEGER,'
-        #           'FOREIGN KEY(list_id) REFERENCES lists(id))')
-        c.execute('SELECT * FROM workingLists')
-        print(c.fetchall())
+        working_list = get_working_list(user, c)
+        values = ', '.join('("%s", %i, %i)' % (i, working_list, user) for i in items)
+        c.execute('INSERT INTO items (name, list_id, request_by) VALUES %s' % values)
+
+
+def remove_items(user, *items):
+    with knibot_db.connect(commit=True) as conn:
+        c = conn.cursor()
+        working_list = get_working_list(user, c)
+        values = ', '.join('"%s"' % i for i in items)
+        c.execute('DELETE FROM items WHERE list_id=%i AND item_name IN (%s)'
+                  % (working_list, values))
+
+
+def remove_all_items(user):
+    with knibot_db.connect(commit=True) as conn:
+        c = conn.cursor()
+        c.execute('DELETE FROM items WHERE list_id=%i' % get_working_list(user, c))
+
+
+def get_list_items(user):
+    with knibot_db.connect() as conn:
+        c = conn.cursor()
+        working_list = get_working_list(user, c)
+        c.execute('SELECT * FROM items WHERE list_id=%i' % working_list)
+        return c.fetchall()
+
+
+# c.execute('CREATE TABLE lists ('
+#           'id INTEGER PRIMARY KEY,'
+#           'name TEXT NOT NULL)')
+# c.execute('CREATE TABLE listsForUsers ('
+#           'list_id INTEGER,'
+#           'user_id INTEGER,'
+#           'admin INTEGER,'
+#           'FOREIGN KEY(list_id) REFERENCES lists(id))')
+# c.execute('CREATE TABLE items ('
+#           'name TEXT NOT NULL,'
+#           'list_id INTEGER,'
+#           'request_by INTEGER,'
+#           'FOREIGN KEY(list_id) REFERENCES lists(id))')
+# c.execute('CREATE TABLE workingLists ('
+#           'user_id INTEGER UNIQUE,'
+#           'list_id INTEGER,'
+#           'FOREIGN KEY(list_id) REFERENCES lists(id))')
+if __name__ == '__main__':
+    with knibot_db.connect(commit=True) as conn:
+        c = conn.cursor()
+        c.execute('DELETE FROM listsForUsers')
+        c.execute('DELETE FROM lists')
+        c.execute('DELETE FROM items')
+        c.execute('DELETE FROM workingLists')
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-from groups import knibot_db
+from groups import *
 import prompts
 
 app = Flask(__name__)
@@ -11,12 +11,38 @@ writing = False
 write_re = re.compile(r'%s\s+([a-zA-Z0-9-_\u05D0-\u05EA]*)' % prompts.write_cmd)
 instead_re = re.compile(r'%s\s+([a-zA-Z0-9-_\u05D0-\u05EA]*)' % prompts.instead_cmd)
 remove_re = re.compile(r'%s\s+([a-zA-Z0-9-_\u05D0-\u05EA]*)' % prompts.remove_cmd)
+list_re = re.compile(r'%s\s+([a-zA-Z0-9-_\u05D0-\u05EA]*)' % prompts.list_cmd)
+new_re = re.compile(r'%s\s+([a-zA-Z0-9-_\u05D0-\u05EA]*)' % prompts.new_cmd)
+
+
+def bot_test(user, incoming_msg):
+    msg = ''
+    try:
+        new_match = new_re.search(incoming_msg)
+        if new_match is not None:
+            list_name = new_match.group(1)
+            create_list(user, list_name)
+            incoming_msg = new_re.sub(list_name, incoming_msg)
+            msg = prompts.new_msg % list_name
+        list_match = list_re.search(incoming_msg)
+        if list_match is not None:
+            list_name = list_match.group(1)
+            set_working_list(user, list_name)
+            msg += prompts.list_msg % list_name if msg == '' else prompts.new_list_msg
+    except Exception as e:
+        msg = str(e)
+    finally:
+        resp = MessagingResponse()
+        resp.message().body(msg)
+        print(msg)
+        return str(resp)
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
     global writing
     incoming_msg = request.values.get('Body', '').lower()
+    user = int(re.search(r'\d+', request.values.get('From')).group())
     resp = MessagingResponse()
     msg = resp.message()
     try:
@@ -26,30 +52,22 @@ def bot():
         elif prompts.help_cmd in incoming_msg:
             msg.body(prompts.help_msg)
         elif prompts.send_cmd in incoming_msg:
-            with knibot_db.connect() as conn:
-                c = conn.cursor()
-                c.execute('SELECT * FROM items')
-                items = '\n'.join(str(i + 1) + ': ' + r[0] + ' (' + str(r[1]) + ')'
-                                  for i, r in enumerate(c.fetchall()))
+            items_raw = get_list_items(user)
+            items = '\n'.join(str(i + 1) + ': ' + r[0] + ' (' + str(r[1]) + ')'
+                              for i, r in enumerate(items_raw))
             msg.body(prompts.send_msg + items if items != '' else 'הרשימה ריקה')
         elif prompts.write_cmd in incoming_msg:
             writing = True
             msg.body(prompts.write_msg)
         elif prompts.bought_cmd in incoming_msg:
-            with knibot_db.connect(commit=True) as conn:
-                c = conn.cursor()
-                c.execute('DELETE FROM items')
+            remove_all_items(user)
             msg.body(prompts.bought_msg)
         elif writing:
             if prompts.finish_cmd in incoming_msg:
                 writing = False
                 msg.body('אוקי')
             else:
-                request_by = int(re.search(r'\d+', request.values.get('From')).group())
-                with knibot_db.connect(commit=True) as conn:
-                    c = conn.cursor()
-                    c.execute('INSERT INTO items (name, request_by) VALUES (?, ?)',
-                              (incoming_msg, request_by))
+                add_items(user, incoming_msg)
         else:
             msg.body(incoming_msg + ' you')
     except Exception as e:
@@ -65,14 +83,4 @@ def bot():
 #   deploy to an actual server
 
 if __name__ == '__main__':
-    # conn = sqlite3.connect('./knibot.db')
-    # c = conn.cursor()
-    # c.execute('DROP TABLE IF EXISTS items')
-    # c.execute('CREATE TABLE items ('
-    #           'name TEXT NOT NULL,'
-    #           'request_by INTEGER)')
-    # c.execute('SELECT * FROM items')
-    # items = c.fetchall()
-    # print('\n'.join(str(i[0]) + ': ' + i[2] + ' (' + i[1] + ')' for i in items))
-    # conn.commit()
-    app.run()
+    print(bot_test(100, 'חדשה חברים'))
