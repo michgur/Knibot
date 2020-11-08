67184969fbba553bd8f746e878dec948265df704
MichGur <michael@thegur.net>
2020-11-08 13:38:17 +0200

    added list & user tables

--- groups.py
+++ groups.py
@@ -27,6 +27,48 @@ class knibot_db:
         return ClosingConnection(cls.path, commit)
 
 
+def create_list(list_name, created_by=0):
+    with knibot_db.connect(commit=True) as conn:
+        c = conn.cursor()
+        c.execute('INSERT INTO lists (name) VALUES ("%s")' % list_name)
+        if created_by != 0:
+            c.execute('INSERT INTO listsForUsers (list_id, user_id, admin) VALUES (?, ?, 1)',
+                      (c.lastrowid, created_by))
+        return c.lastrowid
+
+
+def add_user_to_list(user, list_name, admin=False):
+    with knibot_db.connect(commit=True) as conn:
+        c = conn.cursor()
+        c.execute('SELECT id FROM lists WHERE name="%s"' % list_name)
+        c.execute('INSERT INTO listsForUsers (list_id, user_id, admin) VALUES ('
+                  '?, ?, ?)', (c.fetchall()[0][0], user, 1 if admin else 0))
+
+
 if __name__ == '__main__':
-    with knibot_db.connect() as conn:
-        print(conn, type(conn))
+    # create_list('assholes')
+    # add_user_to_list(1411, 'assholes', True)
+    # add_user_to_list(487834, 'assholes', False)
+    with knibot_db.connect(commit=True) as conn:
+        c = conn.cursor()
+        # c.execute('DROP TABLE IF EXISTS lists')
+        # c.execute('DROP TABLE IF EXISTS listsForUsers')
+        # c.execute('CREATE TABLE lists ('
+        #           'id INTEGER PRIMARY KEY,'
+        #           'name TEXT NOT NULL)')
+        # c.execute('CREATE TABLE listsForUsers ('
+        #           'list_id INTEGER,'
+        #           'user_id INTEGER,'
+        #           'admin INTEGER,'
+        #           'FOREIGN KEY(list_id) REFERENCES lists(id))')
+        # c.execute('INSERT INTO lists (name) VALUES ("friends")')
+        # c.execute('INSERT INTO listsForUsers (list_id, user_id) VALUES (0, 972502057283)')
+        # # select all available lists for a user
+        # c.execute('SELECT name FROM lists WHERE id IN ('
+        #           'SELECT list_id FROM listsForUsers WHERE user_id=972502057283)')
+        # # select all users in a list
+        # c.execute('SELECT user_id FROM listsForUsers WHERE list_id=0')
+        c.execute('SELECT * FROM lists')
+        print('lists: ' + str(c.fetchall()))
+        c.execute('SELECT * FROM listsForUsers')
+        print('listsForUsers: ' + str(c.fetchall()))
