8c80f444acfd930e7c8c177b37d9f189014387b9
MichGur <michael@thegur.net>
2020-11-08 12:34:48 +0200

    moved text prompts to a new module

---
+++ prompts.py
@@ -0,0 +1,22 @@
+greet_msg = '''שלום, אני קניבוט, בוט הקניות.🤖
+אני עוזר לנהל רשימות קניות משותפות בין כמה משתתפים 📝.
+מוצרים שיישלחו אלי ממך יופיעו לכל מי שמשתתף ברשימה שלך, וברגע שייקנו אותם אני אעדכן אותך.
+כמה פקודות לשימוש בי:
+✳️"תרשום"- להוספת פריטים לרשימה
+✳️"שלח"- לשליחת הרשימה המלאה
+✳️"קניתי"- לאיפוס הרשימה
+✳️"תוריד"- להסרת פריטים מהרשימה'''
+help_cmd = 'עזרה'
+help_msg = 'אהלן, אני קניבוט.\n פקודות:\n"עזרה": לקבלת הודעה זו\n"שלח": לשליחת הרשימה\n "תרשום": להוספת מוצרים לרשימה'
+send_cmd = 'שלח'
+send_msg = 'הנה הרשימה:\n'
+write_cmd = 'תרשום'
+instead_cmd = 'במקום'
+write_msg = 'אני רושם 📝. בסיום הרשימה נא לשלוח "זהו"'
+finish_cmd = 'זהו'
+failed_msg = 'אופס חלה אצלי תקלה'
+bought_cmd = 'קניתי'
+bought_msg = 'סגור איפסתי את הרשימה'
+remove_cmd = 'תוריד'
+remove_msg = 'הסרתי את הפריטים'
+debug_cmd = 'דבג'
